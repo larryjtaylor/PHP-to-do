158dd0e95d866596ecca212219473dc8c5130650
Calla Rudolph and Nathan Stewart <nathan@>
2017-06-27 14:28:31 -0700

    add twig dependency and twig path

--- app/app.php
+++ app/app.php
@@ -11,6 +11,10 @@
 
     $app = new Silex\Application();
 
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' =>__DIR__.'/../views'
+    ));
+
     $app->get("/", function() {
 
         $output = "";
